7736815ea3af2c3bd0ab8c2ea5772d34d2a19fbf
AskariyanKarine <52951419+AskariyanKarine@users.noreply.github.com>
2019-11-22 16:45:28 +0300

    Add files via upload

--- HW_5/task6.cpp
+++ HW_5/task6.cpp
@@ -5,48 +5,43 @@
 #include <map> 
 using namespace std;
 
-multimap<int, char> counted_str(string message)
+map<string, int> counted_str(string& message)
 {
-    string s, str;
-    int x, y;
-    map <char, int> a;
-    multimap<int, char> sortedPaths;
-    map <char,int> res;
-    //getline(cin, str);
-    if (message.size()==0){
-      cout << "\n";
+  string s, str;
+  int x, y;
+  map <string, int> a;
+  multimap<int, string> sortedPaths;
+  map <string, int> res;
+  getline(cin, str);
+  if (str.size()==0)
+    cout << "\n";
+  for (int i = 0; str[i]; i++) {
+    if (isalpha(str[i]))
+      s += tolower(str[i]);
+  }
+  sort(s.begin(), s.end());    
+  x = y = 0;
+  for (int i = y; s[i]; i++) {
+    if (s[i] == s[i+1]) {
+      x++;
     }
-    for (int i = 0; message[i]; i++)
-    {
-        if (isalpha(str[i]))
-            s += tolower(message[i]);
+    else {
+      y = x;
+      x++;
+      a.insert ( pair<string,int>(string(1, s[i]),x) );
+      //cout << s[i] << ": " << x << endl;
+      x = 0;
+      }
     }
-    sort(s.begin(), s.end());
-    x = y = 0;
-    for (int i = y; s[i]; i++)
-    {
-        if (s[i] == s[i+1])
-        {
-            x++;
-        }
-        else
-        {
-            y = x;
-            x++;
-             a.insert ( pair<char,int>(s[i],x) );
-            //cout << s[i] << ": " << x << endl;
-            x = 0;
-        }
-    }
-    for(auto it = a.rbegin(); it != a.rend(); ++it)
-    {
+  for(auto it = a.rbegin(); it != a.rend(); ++it) {
         int key = (*it).second;
-        char val = (*it).first;
-        sortedPaths.insert(pair<int, char>(key, val));
+        string val = (*it).first;
+        sortedPaths.insert(pair<int, string>(key, val));
     }
     
-    //for(auto it = sortedPaths.rbegin(); it != sortedPaths.rend(); //++it)
-      //cout << " " << (*it).first << " : " << (*it).second << endl;
-
-  return sortedPaths;
+  for(auto it = sortedPaths.rbegin(); it!=sortedPaths.rend(); ++it){
+    cout << " " << (*it).second << " : " << (*it).first << endl;
+    res.insert(pair<string, int>((*it).second, (*it).first));
+  }
+  return res;
 }
